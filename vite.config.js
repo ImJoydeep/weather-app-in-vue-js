4a5289506a04c32413479d5ca87b157e38b12ce7
ImJoydeep <jbaidya42@gmail.com>
2023-04-20 13:11:12 +0530

    configure vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/d
-export default defineConfig({import {  } from "module";
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
